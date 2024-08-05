122f457cb109cd4760aa68ec02a043c786d158a9
Alain Irebe Gashumba <115883068+irebealain@users.noreply.github.com>
2024-08-05 16:14:45 +0300

    new changes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def days_to_units(num_of_days):
     if num_of_days > 0:
         return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
 
-user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
+
 def validate_and_execute():
     try:
         
@@ -17,4 +17,7 @@ def validate_and_execute():
             print("You entered a 0, please enter a valid positive")
     except ValueError:
         print("Your input is not a valid number. Don't ruin my program!")
-validate_and_execute()
+
+while True:
+    user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
+    validate_and_execute()
