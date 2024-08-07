f2b44250e51625155e92a8aeab9e90dcb0b345bb
Alain Irebe Gashumba <115883068+irebealain@users.noreply.github.com>
2024-08-07 12:01:12 +0300

    New changes

--- invetory.py
+++ invetory.py
@@ -1,3 +1,14 @@
 import openpyxl
 
-wb = openpyxl.Workbook()
+inv_file = openpyxl.load_workbook("inventory.xlsx")
+product_list = inv_file["Sheet1"]
+
+products_per_supplier = {}
+
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4)
+    # if supplier_name not in products_per_supplier:
+    #     current_num_products = products_per_supplier[supplier_name]
+    #     products_per_supplier[supplier_name] = current_num_products + 1
+    # else:
+    #     products_per_supplier[supplier_name] = 1
